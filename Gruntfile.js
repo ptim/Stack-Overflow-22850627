d16faa1042c0085bd34cf3bf129f4c798421dceb
Tim Osborn <tim@memelab.com.au>
2014-06-03 00:38:18 +1000

    (my port 9000 was taken by another service…)

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,7 +47,7 @@ module.exports = function (grunt) {
     },
     connect: {
       options: {
-        port: 9000,
+        port: 9001,
         // change this to '0.0.0.0' to access the server from outside
         hostname: 'localhost',
         livereload: 35729
